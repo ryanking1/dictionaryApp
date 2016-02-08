4a304ab1bf5314734e0ab3b247ee089ea40e9eca
kevin mattison and mike schennum <student@epicodus.com>
2016-02-08 11:49:22 -0800

    adds html template

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -1,21 +1,32 @@
+import java.util.HashMap;
 import java.util.ArrayList;
 
-public class PingPong {
-  public static void main(String[] args) {}
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
 
-    public ArrayList isPingPong(Integer number) {
-      ArrayList<Object> numberArray = new ArrayList<Object>();
-      for (Integer i = 1; i <= number; i++) {
-        if (i % 15 == 0) {
-          numberArray.add("pingpong");
-        } else if (i % 3 == 0) {
-          numberArray.add("ping");
-        } else if (i % 5 == 0) {
-          numberArray.add("pong");
-        } else {
-          numberArray.add(i);
-        }
-      }
-    return numberArray;
+import static spark.Spark.*;
+
+public class PingPong {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    get("/", (request, response) -> {
+      return new ModelAndView(new HashMap(), "templates/pingpong.vtl");
+    }, new VelocityTemplateEngine());
+
+  //   public ArrayList isPingPong(Integer number) {
+  //     ArrayList<Object> numberArray = new ArrayList<Object>();
+  //     for (Integer i = 1; i <= number; i++) {
+  //       if (i % 15 == 0) {
+  //         numberArray.add("pingpong");
+  //       } else if (i % 3 == 0) {
+  //         numberArray.add("ping");
+  //       } else if (i % 5 == 0) {
+  //         numberArray.add("pong");
+  //       } else {
+  //         numberArray.add(i);
+  //       }
+  //     }
+  //   return numberArray;
+  // }
   }
 }
