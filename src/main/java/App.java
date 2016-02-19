0cd016187db59d057625053ee3487be194c657e9
Ryan <ryanking157@hotmail.com>
2016-02-19 10:21:46 -0800

    add funcional index and add word forms, but add new definitions still broken

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,72 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("words", Word.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/wordform", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/wordform.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/definitionForm", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        model.put("template", "templates/definitionForm.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+    post("/word", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String word = request.queryParams("word");
+      Word newWord = new Word(word);
+      model.put("words", Word.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Word word = Word.find(Integer.parseInt(request.params(":id")));
+      model.put("word", word);
+      model.put("template", "templates/word-definition.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/:id/definitionForm", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        Word word = Word.find(Integer.parseInt(request.params(":id")));
+        ArrayList<Definition> definitions = word.getDefinitions();
+        model.put("word", word);
+        model.put("definitions", definitions);
+        model.put("template", "templates/definitionForm.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+    post("/definition", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+
+        Word word = Word.find(Integer.parseInt(request.queryParams("wordId")));
+        ArrayList<Definition> definitions = word.getDefinitions();
+
+        if (definitions == null) {
+          definitions = new ArrayList<Definition>();
+          request.session().attribute("definitions", definitions);
+        }
+
+        String definition = request.queryParams("definition");
+
+        Definition newDefinition = new Definition(definition);
+
+        definitions.add(newDefinition);
+        model.put("definitions", definitions);
+        model.put("template", "templates/word-definition.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
 
 
   }
