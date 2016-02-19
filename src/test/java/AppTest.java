2a5f57548fdeadbe53c2283c8ae20bee37de1b33
Ryan <ryanking157@hotmail.com>
2016-02-19 11:08:14 -0800

    add passing integrations tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -22,4 +22,39 @@ public class AppTest extends FluentTest {
     goTo("http://localhost:4567/");
     assertThat(pageSource()).contains("Word List");
   }
+
+  @Test
+  public void checkWordIsAddedCorrectly() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Add a New Word"));
+    fill("#word").with("cat");
+    submit(".btn", withText("Add Word"));
+    assertThat(pageSource()).contains("cat");
+  }
+
+  @Test
+  public void checkMultipleWordsAreAddedCorrectly() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Add a New Word"));
+    fill("#word").with("cat");
+    submit(".btn", withText("Add Word"));
+    assertThat(pageSource()).contains("cat");
+    click("a", withText("Add a New Word"));
+    fill("#word").with("dog");
+    submit(".btn", withText("Add Word"));
+    assertThat(pageSource()).contains("cat", "dog");
+  }
+
+  @Test
+  public void checkWordContainsDefinition() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Add a New Word"));
+    fill("#word").with("testcat");
+    submit(".btn", withText("Add Word"));
+    click("a", withText("testcat"));
+    click("a", withText("Add a new definition"));
+    fill("#definition").with("fluffy ball of hatred");
+    submit(".btn", withText("Add Definition"));
+    assertThat(pageSource()).contains("fluffy ball of hatred");
+  }
 }
