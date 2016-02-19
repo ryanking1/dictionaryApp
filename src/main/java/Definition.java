2d2e0af6904dbbfd9b735d75d82971105c5a74b4
Ryan <ryanking157@hotmail.com>
2016-02-19 08:23:35 -0800

    add class and constructor for definitions

---
+++ src/main/java/Definition.java
@@ -0,0 +1,12 @@
+public class Definition {
+
+  private static ArrayList<Definition> instances = new ArrayList<Definition>();
+
+  private String mDefinition;
+  private int mId;
+
+  public Definition(String definition) {
+    mDefinition = definition;
+    instances.add(this);
+    mId = instances.size();
+  }
