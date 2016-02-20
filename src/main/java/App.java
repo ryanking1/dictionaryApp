fc843939ab4745e6322c597f578f980c968b1e84
Ryan <ryanking157@hotmail.com>
2016-02-19 16:14:47 -0800

    add clear functionality and fix some minor indentation issues

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,6 +17,14 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/clear", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Word.all().clear();
+      model.put("words", Word.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     get("/wordform", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/wordform.vtl");
@@ -24,10 +32,10 @@ public class App {
     }, new VelocityTemplateEngine());
 
     get("/definitionForm", (request, response) -> {
-        HashMap<String, Object> model = new HashMap<String, Object>();
-        model.put("template", "templates/definitionForm.vtl");
-        return new ModelAndView(model, layout);
-      }, new VelocityTemplateEngine());
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/definitionForm.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
     post("/word", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
@@ -47,37 +55,35 @@ public class App {
     }, new VelocityTemplateEngine());
 
     get("/:id/definitionForm", (request, response) -> {
-        HashMap<String, Object> model = new HashMap<String, Object>();
-        Word word = Word.find(Integer.parseInt(request.params(":id")));
-        ArrayList<Definition> definitions = word.getDefinitions();
-        model.put("word", word);
-        model.put("definitions", definitions);
-        model.put("template", "templates/definitionForm.vtl");
-        return new ModelAndView(model, layout);
-      }, new VelocityTemplateEngine());
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Word word = Word.find(Integer.parseInt(request.params(":id")));
+      ArrayList<Definition> definitions = word.getDefinitions();
+      model.put("word", word);
+      model.put("definitions", definitions);
+      model.put("template", "templates/definitionForm.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
     post("/definition", (request, response) -> {
-        HashMap<String, Object> model = new HashMap<String, Object>();
+      HashMap<String, Object> model = new HashMap<String, Object>();
 
-        Word word = Word.find(Integer.parseInt(request.queryParams("wordId")));
-        ArrayList<Definition> definitions = word.getDefinitions();
+      Word word = Word.find(Integer.parseInt(request.queryParams("wordId")));
+      ArrayList<Definition> definitions = word.getDefinitions();
 
-        if (definitions == null) {
-          definitions = new ArrayList<Definition>();
-          request.session().attribute("definitions", definitions);
-        }
+      if (definitions == null) {
+        definitions = new ArrayList<Definition>();
+        request.session().attribute("definitions", definitions);
+      }
 
-        String definition = request.queryParams("definition");
+      String definition = request.queryParams("definition");
 
-        Definition newDefinition = new Definition(definition);
+      Definition newDefinition = new Definition(definition);
 
-        definitions.add(newDefinition);
-        model.put("word", word);
-        model.put("definitions", definitions);
-        model.put("template", "templates/word-definition.vtl");
-        return new ModelAndView(model, layout);
-      }, new VelocityTemplateEngine());
-
-
+      definitions.add(newDefinition);
+      model.put("word", word);
+      model.put("definitions", definitions);
+      model.put("template", "templates/word-definition.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
